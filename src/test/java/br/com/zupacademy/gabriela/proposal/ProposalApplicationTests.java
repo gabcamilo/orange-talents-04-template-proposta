9f0b5a37ae02f4f1000aab78a295f87922b87b87
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-05-11 20:43:42 -0300

    Application setup

---
+++ src/test/java/br/com/zupacademy/gabriela/proposal/ProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.gabriela.proposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
